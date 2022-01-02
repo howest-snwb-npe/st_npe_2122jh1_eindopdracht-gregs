eb91e413e35e58c9e79817d94d9aa8932fc9bcb0
Gregory Sideris <gregory.sideris@student.howest.be>
2022-01-02 21:38:19 +0100

    acolyte as first npc monster

--- game.py
+++ game.py
@@ -1,1 +1,14 @@
-#test
+#Imports
+import requests
+import json
+import random
+
+
+#monster import acolyte
+base_url = "https://www.dnd5eapi.co"
+r = requests.get(base_url+"/api/monsters/acolyte")
+if not r.status_code == 200:
+     raise Exception("Incorrect reply from D&D API. Status code: {}. Text: {}".format(r.status_code, r.text))
+monster = r.json()
+
+print(monster)
