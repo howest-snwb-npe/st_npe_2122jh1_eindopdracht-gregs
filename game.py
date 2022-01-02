6c84e30c6d4b658d8c8e583638ee71914e8079df
Gregory Sideris <gregory.sideris@student.howest.be>
2022-01-02 22:04:09 +0100

    eerst diagoloog met npc

--- game.py
+++ game.py
@@ -10,11 +10,15 @@ r = requests.get(base_url+"/api/monsters/acolyte")
 if not r.status_code == 200:
      raise Exception("Incorrect reply from D&D API. Status code: {}. Text: {}".format(r.status_code, r.text))
 monster = r.json()
-
+print(monster)
 #basic stats character
 
 life = 100
 attack = 1
+
+startsword = 5
+ironsword = 10
+silversword = 20
 
 ######################
 #### Functions #######
@@ -27,7 +31,8 @@ def sprint(str):
      sys.stdout.flush()
      time.sleep(3./90)
 
-
+acolytehealth = (monster.get('hit_points'))
+# print(acolytehealth)
 
 
 
@@ -40,8 +45,33 @@ def sprint(str):
 #################
 
 
+# dialogue
+
 sprint('You wake up in a dark room')
-sprint('next to you is a sword do you grab it? ')
+sprint('next to you is a sword do you grab it? (yes/no) ')
+
+swordgrab = input()
+if swordgrab == "yes":
+     attack = attack + startsword
+     sprint('you grab the sword and hold it close while moving outside of the room')
+     print('your attack increases and is now ' + str(attack))
+else:
+     sprint('you ignore the sword and move outside the room')
+
+sprint('suddenly an acolyte appears and yells at you in a foreign language')
+print('will you attack or try to talk to the acolyte? (attack/talk')
+
+acolytedialogue = input()
+
+if acolytedialogue == "attack":
+     print('k')
+else:
+     print('hi who are you? ')
+
+
+# first battle
+
+
 
 
         
