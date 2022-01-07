1b88977b326310d6f49a228bf2b0a3f97f102c2a
Gregory Sideris <gregory.sideris@student.howest.be>
2022-01-07 20:46:18 +0100

    name change -> slow to functions

--- game.py
+++ game.py
@@ -3,7 +3,7 @@ import requests
 import json
 import random
 import sys,time
-from Functions import slowprint
+from Functions import Functions
 from ascii_art import AscIIArt
 
 
@@ -75,50 +75,50 @@ shoplist = ('''
 #### CHAPTER 1
 # dialogue with 1st NPC 
 
-slowprint.sprint('You wake up in a dark room')
-slowprint.sprint('next to you is a sword do you grab it? (yes/no) ')
+Functions.sprint('You wake up in a dark room')
+Functions.sprint('next to you is a sword do you grab it? (yes/no) ')
 
 swordgrab = input().lower()
-while swordgrab not in['yes', 'no']:
-     slowprint.sprint('your quest cannot continue with your correct input (yes/no) ')
-     swordgrab = input().lower()
+
+
+
 if swordgrab == "yes":
      attack = attack + startsword
-     slowprint.sprint('you grab the sword and hold it close while moving outside of the room')
+     Functions.sprint('you grab the sword and hold it close while moving outside of the room')
      print('your attack increases and is now ' + str(attack))
 else:
-     slowprint.sprint('you ignore the sword and move outside the room')
+     Functions.sprint('you ignore the sword and move outside the room')
 
-slowprint.sprint('suddenly an acolyte appears and yells at you in a foreign language')
+Functions.sprint('suddenly an acolyte appears and yells at you in a foreign language')
 print('will you attack or try to talk to the acolyte? (attack/talk) ')
 
 #validation
 acolytedialogue = input().lower()
 while acolytedialogue not in['attack', 'talk']:
-     slowprint.sprint('your quest cannot continue with your correct input (attack/talk) ')
+     Functions.sprint('your quest cannot continue with your correct input (attack/talk) ')
      acolytedialogue = input().lower()
 if acolytedialogue == "attack":
-          slowprint.sprint('you launch a quick sneak attack against the weird acolyte and you deal critical damage! ')
+          Functions.sprint('you launch a quick sneak attack against the weird acolyte and you deal critical damage! ')
           print(AscIIArt.zwaardanimatie)
           currentattack = attack * 2
-          slowprint.sprint('the acolyte his healthpoints are ' + str(acolytehealth))
-          slowprint.sprint('your quick attack hits for ' + str(currentattack))
+          Functions.sprint('the acolyte his healthpoints are ' + str(acolytehealth))
+          Functions.sprint('your quick attack hits for ' + str(currentattack))
           remainingmobhealth = acolytehealth - currentattack
           if remainingmobhealth < 0:
                print('you killed the acolyte')
-               slowprint.sprint('Going through the clothes of the acolyte you find 5 gold')
+               Functions.sprint('Going through the clothes of the acolyte you find 5 gold')
                gold = gold + 5 
           else:
                print('')
 
 elif acolytedialogue == "talk":
-          slowprint.sprint('hi who are you? ')
+          Functions.sprint('hi who are you? ')
      #verder te doen
 
 
 
 # Game Chapter 2 
-slowprint.sprint('after killing the acolyte you think about your next steps, should you first check your current status or go through the next door? status/door ')
+Functions.sprint('after killing the acolyte you think about your next steps, should you first check your current status or go through the next door? status/door ')
 chapter2 = input()
 if chapter2 == 'status':
      print('current attack is ' + str(attack))
@@ -127,27 +127,27 @@ if chapter2 == 'status':
 else:
      pass
 
-slowprint.sprint('while passing through the door you come to an open court , in the distance you see a tower')
+Functions.sprint('while passing through the door you come to an open court , in the distance you see a tower')
 print(AscIIArt.dungeontower)
 
-slowprint.sprint('while being amazed at the sight you also see something to your left (tower/left) ')
+Functions.sprint('while being amazed at the sight you also see something to your left (tower/left) ')
 chapter2dialogue = input().lower()
 while chapter2dialogue not in['tower', 'left']:
-     slowprint.sprint('your quest cannot continue without your correct input (tower/left) ')
+     Functions.sprint('your quest cannot continue without your correct input (tower/left) ')
      chapter2dialogue = input().lower()
      if chapter2dialogue == "tower":
-          slowprint.sprint('you carefully walk towards the tower')
+          Functions.sprint('you carefully walk towards the tower')
      else:
-          slowprint.sprint('you turn to your left and walk towards a house in the distance')
+          Functions.sprint('you turn to your left and walk towards a house in the distance')
           print(AscIIArt.shopkeeper)
-          slowprint.sprint('you enter the shop and find a bunch of items for sale! ')
+          Functions.sprint('you enter the shop and find a bunch of items for sale! ')
           print(shoplist)
           shopinput = input().lower
           while shopinput not in['1', '2', '3']:
-               slowprint.sprint('your quest cannot continue with your correct input (tower/left) ')
+               Functions.sprint('your quest cannot continue with your correct input (tower/left) ')
                chapter2dialogue = input().lower()
           if shopinput == '3':
-               slowprint.sprint('you turn to your left and walk towards a house in the distance')
+               Functions.sprint('you turn to your left and walk towards a house in the distance')
           else:
                print("nog grammed yet")
 
